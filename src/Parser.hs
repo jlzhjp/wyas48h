0752e95d0dabe9ad60d830b979f4a2137e8c54ce
Akari <jvjdev@gmail.com>
2025-03-07 20:22:48 +0800

    exercise 2.1-2.3

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,7 +1,7 @@
 module Parser (doParse, LispVal (..)) where
 
 import Control.Applicative ((<|>))
-import Text.Parsec (ParseError, try)
+import Text.Parsec (ParseError, try, anyChar, unexpected)
 import Text.ParserCombinators.Parsec
   ( Parser,
     char,
@@ -33,24 +33,35 @@ symbol = oneOf "!$%&|*+-/:<=>?@^_~#"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+-- exercise 2.2, 2.3: handle escaped characters in strings
 parseString :: Parser LispVal
-parseString =
-  do
-    _ <- char '"'
-    x <- many (noneOf "\"")
-    _ <- char '"'
-    return $ String x
+parseString = do
+  _ <- char '"'
+  x <- many (escaped <|> noneOf "\"")
+  _ <- char '"'
+  return $ String x
+  where
+    escaped:: Parser Char
+    escaped = do
+      _ <- char '\\'
+      c <- anyChar
+      case c of
+        '"' -> return '"'
+        't' -> return '\t'
+        'n' -> return '\n'
+        'r' -> return '\r'
+        '\\' -> return '\\'
+        _ -> unexpected $ "escape sequence \\" ++ [c]
 
 parseAtom :: Parser LispVal
-parseAtom =
-  do
-    first <- letter <|> symbol
-    rest <- many (letter <|> digit <|> symbol)
-    let atom = first : rest
-    return $ case atom of
-      "#t" -> Bool True
-      "#f" -> Bool False
-      _ -> Atom atom
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
 
 parseDottedList :: Parser LispVal
 parseDottedList = do
@@ -58,8 +69,11 @@ parseDottedList = do
   listTail <- char '.' >> spaces >> parseExpr
   return $ DottedList listHead listTail
 
+-- exercise 2.1 rewrite parseNumber using: 1. do-notation; 2. >>=
 parseNumber :: Parser LispVal
-parseNumber = Number . read <$> many1 digit
+parseNumber = do
+  digitStr <- many1 digit
+  return $ Number . read $ digitStr
 
 parseList :: Parser LispVal
 parseList = List <$> sepBy parseExpr spaces
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,8 +1,14 @@
 module ParserSpec (parserSpec) where
 
-import Test.Hspec (shouldBe, describe, it, Spec, hspec)
-import Text.Parsec (parse, ParseError)
-import Lib (doParse, LispVal(..))
+import Lib (LispVal (..), doParse)
+import Test.Hspec
+  ( Spec,
+    describe,
+    expectationFailure,
+    it,
+    shouldBe,
+    shouldContain,
+  )
 
 parserSpec :: Spec
 parserSpec = do
@@ -10,74 +16,96 @@ parserSpec = do
     describe "parseAtom" $ do
       it "parses a simple atom" $
         doParse "abc" `shouldBe` Right (Atom "abc")
-      
+
       it "parses an atom with symbols" $
         doParse "a-b_c!$%&" `shouldBe` Right (Atom "a-b_c!$%&")
-      
+
       it "parses boolean true" $
         doParse "#t" `shouldBe` Right (Bool True)
-      
+
       it "parses boolean false" $
         doParse "#f" `shouldBe` Right (Bool False)
-    
+
     describe "parseString" $ do
       it "parses a simple string" $
         doParse "\"hello\"" `shouldBe` Right (String "hello")
-      
+
       it "parses an empty string" $
         doParse "\"\"" `shouldBe` Right (String "")
-      
+
       it "parses a string with spaces" $
         doParse "\"hello world\"" `shouldBe` Right (String "hello world")
-    
+
+      it "parses a string with an escaped quotation mark" $
+        doParse "\"hello\\\"world\"" `shouldBe` Right (String "hello\"world")
+
+      it "parses a string with multiple escaped quotation marks" $
+        doParse "\"he said: \\\"hello\\\"\"" `shouldBe` Right (String "he said: \"hello\"")
+
+      it "fails when string contains unknown escape sequence" $
+        case doParse "\"bad\\zescape\"" of
+          Left err -> show err `shouldContain` "escape sequence \\z"
+          Right _ -> expectationFailure "expected parse to fail"
+
     describe "parseNumber" $ do
       it "parses a single digit" $
         doParse "5" `shouldBe` Right (Number 5)
-      
+
       it "parses multiple digits" $
         doParse "123" `shouldBe` Right (Number 123)
-    
+
     describe "parseList" $ do
       it "parses an empty list" $
         doParse "()" `shouldBe` Right (List [])
-      
+
       it "parses a list with one element" $
         doParse "(a)" `shouldBe` Right (List [Atom "a"])
-      
+
       it "parses a list with multiple elements" $
-        doParse "(a b c)" `shouldBe` 
-          Right (List [Atom "a", Atom "b", Atom "c"])
-      
+        doParse "(a b c)"
+          `shouldBe` Right (List [Atom "a", Atom "b", Atom "c"])
+
       it "parses a nested list" $
-        doParse "(a (b c))" `shouldBe` 
-          Right (List [Atom "a", List [Atom "b", Atom "c"]])
-    
+        doParse "(a (b c))"
+          `shouldBe` Right (List [Atom "a", List [Atom "b", Atom "c"]])
+
     describe "parseDottedList" $ do
       it "parses a simple dotted list" $
-        doParse "(a . b)" `shouldBe` 
-          Right (DottedList [Atom "a"] (Atom "b"))
-      
+        doParse "(a . b)"
+          `shouldBe` Right (DottedList [Atom "a"] (Atom "b"))
+
       it "parses a dotted list with multiple head elements" $
-        doParse "(a b c . d)" `shouldBe` 
-          Right (DottedList [Atom "a", Atom "b", Atom "c"] (Atom "d"))
-    
+        doParse "(a b c . d)"
+          `shouldBe` Right (DottedList [Atom "a", Atom "b", Atom "c"] (Atom "d"))
+
     describe "parseQuoted" $ do
       it "parses a quoted atom" $
-        doParse "'a" `shouldBe` 
-          Right (List [Atom "quote", Atom "a"])
-      
+        doParse "'a"
+          `shouldBe` Right (List [Atom "quote", Atom "a"])
+
       it "parses a quoted list" $
-        doParse "'(a b c)" `shouldBe` 
-          Right (List [Atom "quote", List [Atom "a", Atom "b", Atom "c"]])
-    
+        doParse "'(a b c)"
+          `shouldBe` Right (List [Atom "quote", List [Atom "a", Atom "b", Atom "c"]])
+
     describe "complex expressions" $ do
       it "parses a complex expression" $
-        doParse "(define (factorial n) (if (= n 0) 1 (* n (factorial (- n 1)))))" `shouldBe`
-          Right (List [Atom "define", 
-                      List [Atom "factorial", Atom "n"], 
-                      List [Atom "if", 
-                           List [Atom "=", Atom "n", Number 0], 
-                           Number 1, 
-                           List [Atom "*", Atom "n", 
-                                List [Atom "factorial", 
-                                     List [Atom "-", Atom "n", Number 1]]]]])
+        doParse "(define (factorial n) (if (= n 0) 1 (* n (factorial (- n 1)))))"
+          `shouldBe` Right
+            ( List
+                [ Atom "define",
+                  List [Atom "factorial", Atom "n"],
+                  List
+                    [ Atom "if",
+                      List [Atom "=", Atom "n", Number 0],
+                      Number 1,
+                      List
+                        [ Atom "*",
+                          Atom "n",
+                          List
+                            [ Atom "factorial",
+                              List [Atom "-", Atom "n", Number 1]
+                            ]
+                        ]
+                    ]
+                ]
+            )
