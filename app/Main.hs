66ad30f525d8598d3dc87261456722ba452190fb
Akari <jvjdev@gmail.com>
2025-03-09 16:34:23 +0800

    implement REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,36 @@
 module Main (main) where
 
-import Lib (eval, doParse)
 import Control.Monad.Except (runExcept)
+import Lib (doParse, eval)
 import System.Environment (getArgs)
+import System.IO (hFlush, stdout)
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ either (\x -> "Error: " ++ show x) show $ runExcept (doParse expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: (Monad m) => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ predicate prompt action = do
+  result <- prompt
+  if predicate result
+    then return ()
+    else action result >> until_ predicate prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
 
 main :: IO ()
 main = do
-  input <- parseArgs <$> getArgs
-  putStrLn $ formatResult $ runExcept (doParse input >>= eval)
-  where
-    parseArgs args = if null args then "()" else head args
-    formatResult = either (\err -> "Error: " ++ show err) show
+  args <- getArgs
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ head args
+    _ -> putStrLn "Program takes only 0 or 1 argument"
